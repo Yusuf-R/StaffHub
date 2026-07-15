e11d316a6da48b00583b08e9fecf91e498a92c40
Yusuf <y.abdulwasiu@gmail.com>
2026-07-15 02:31:51 +0100

    feat: :sparkles: StaffHub Boiler Plate

---
+++ src/main/java/com/naviroq/staffhub/StaffHubApplication.java
@@ -0,0 +1,13 @@
+package com.naviroq.staffhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaffHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naviroq/staffhub/StaffHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naviroq.staffhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
